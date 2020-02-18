3d27a7bd5628e6812c2e2fbe3be920240a75db07
carlosanguix <carlos.anguix@gmail.com>
2020-02-18 12:25:53 +0100

    gitignore

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Imports
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 // Configuraciones
 const app = express();
 const port = process.env.port || 3003;
+app.use(bodyParser.json())
 
 // Servicio al cliente /public
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Arrancando el servidor
 app.listen(port, () => {
--- public/index.js
+++ public/index.js
@@ -1,9 +1,50 @@
-function init() {
-    
-    console.log('index.html');
+function extraerUrlPokemons() {
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1000')
+        .then(res => {
+            return res.json();
+        }).then(respuesta => {
+            respuesta.results.forEach(pokemon => {
+                urlPokemons.push(pokemon.url);
+            });
+        }).then(() => {
+            extraerPokemons();
+        });
 }
 
+function extraerPokemons() {
 
+    for (let i = 0; i < urlPokemons.length; i++) {
 
+        fetch(urlPokemons[i])
+            .then(res => {
+                return res.json();
+            }).then(poke => {
+                let pokemon = {
+                    name: poke.forms[0].name,
+                    types: poke.types,
+                    moves: [poke.moves[0], poke.moves[1], poke.moves[2], poke.moves[3]],
+                }
+                pokemons.push(pokemon);
+            })
+    }
+    setTimeout(pintarPokemons, 5000)
+}
 
+function pintarPokemons() {
+
+    console.log(pokemons.length);
+    
+    for (let i = 0; i < pokemons.length; i++) {
+        
+    }
+}
+
+function init() {
+
+    extraerUrlPokemons();
+}
+
+let pokemons = [];
+let urlPokemons = [];
 window.onload = init;
